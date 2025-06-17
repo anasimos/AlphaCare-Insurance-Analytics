8e2c28d516038bd49731619f5da5d168b7ff5415
anasimos <anasimostesfaye@gmail.com>
2025-06-17 13:23:24 +0300

    feat: Completed Task 1 EDA and initial setup

---
+++ src/eda.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# --- Configuration and Data Loading ---
+# DATA_PATH should point to your .txt file now.
+# Please ensure your 503 MB .txt file is placed in a 'data' folder
+# within your project directory and named 'MachineLearningRating_v3.txt'.
+
+
